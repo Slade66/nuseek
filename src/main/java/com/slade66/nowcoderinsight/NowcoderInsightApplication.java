a3297ef0803a92773fcf2435ca9933ff9802389b
slade <slade66@tutamail.com>
2025-05-10 17:21:23 +0800

    初始化项目

---
+++ src/main/java/com/slade66/nowcoderinsight/NowcoderInsightApplication.java
@@ -0,0 +1,13 @@
+package com.slade66.nowcoderinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowcoderInsightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NowcoderInsightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/slade66/nowcoderinsight/NowcoderInsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slade66.nowcoderinsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowcoderInsightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
